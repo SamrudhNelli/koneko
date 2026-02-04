a8c3c5e56f5020492782acb901fec014825e598c
Samrudh Nelli <samrudhnelli@gmail.com>
2026-02-05 02:54:49 +0530

    implement a basic structure of the wayland code

--- go.mod
+++ go.mod
@@ -2,13 +2,9 @@ module koneko
 
 go 1.25.6
 
-require (
-	github.com/diamondburned/gotk4-layer-shell/pkg v0.0.0-20240109211357-6efa9f6dc438
-	github.com/diamondburned/gotk4/pkg v0.3.1
-)
+require github.com/neurlang/wayland v0.3.0
 
 require (
-	github.com/KarpelesLab/weak v0.1.1 // indirect
-	go4.org/unsafe/assume-no-moving-gc v0.0.0-20231121144256-b99613f794b6 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	github.com/yalue/native_endian v1.0.2 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,121 +1,84 @@
 package main
 
 import (
-	"bytes"
-	_ "embed"
-	"image"
-	"image/png"
-	_ "image/png"
-	"koneko/source/hypr"
-	"koneko/source/game"
 	"log"
-	"os"
 
-	"github.com/diamondburned/gotk4-layer-shell/pkg/gtk4layershell"
-	"github.com/diamondburned/gotk4/pkg/gdk/v4"
-	"github.com/diamondburned/gotk4/pkg/glib/v2"
-	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+	"koneko/protocols/wlr"
+
+	"github.com/neurlang/wayland/wl"
+	"github.com/neurlang/wayland/wlclient"
 )
 
-//go:embed assets/koneko.png
-var imgBytes []byte
-
-func imageToTexture(img image.Image) *gdk.Texture {
-	var buff bytes.Buffer
-	err := png.Encode(&buff, img) 
-	if err != nil {
-		log.Fatal(err)
-	}
-	bytes := glib.NewBytes(buff.Bytes())
-	texture, err := gdk.NewTextureFromBytes(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return texture
-}
-
-func cropImage(img image.Image, x, y, w, h int) image.Image {
-	sub, ok := img.(interface {
-		SubImage(r image.Rectangle) image.Image
-	})
-	if ok {
-		return sub.SubImage(image.Rect(x, y, x + w, y + h))
-	}
-	return img
-}
-
-func transparentWindow(window *gtk.Window) {
-	css := "window { background-color: rgba(0,0,0,0); }"
-	provider := gtk.NewCSSProvider()
-	provider.LoadFromData(css)
-	display := gdk.DisplayGetDefault()
-	gtk.StyleContextAddProviderForDisplay(display, provider, gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+type winState struct {
+	appID string
+	title string
+	// pImage *image.RGBA
+	width, height int32
+	// frame *image.RGBA
+	exit bool
+	display *wl.Display
+	registry *wl.Registry
+	shm *wl.Shm
+	zwlr *wlr.ZwlrLayerShellV1
+	compositor *wl.Compositor
+	// wmBase *xdg.WmBase
+	seat *wl.Seat
+	surface *wl.Surface
+	// xdgSurface  *xdg.Surface
+	// xdgTopLevel *xdg.Toplevel
+	keyboard *wl.Keyboard
+	pointer *wl.Pointer
 }
 
 func main() {
+	window := &winState{
+		title: "koneko",
+		appID: "koneko",
+	}
 
-	app := gtk.NewApplication("com.koneko", 0)
-	app.ConnectActivate(func() {
+	display, err := wl.Connect("")
+	if err != nil {
+		log.Fatalf("Unable to connect to wayland server! %v", err)
+	}
+	window.display = display
+	display.AddErrorHandler(window)
+	
+	run(window)
 
-		window := gtk.NewWindow()
-		app.AddWindow(window)
-		gtk4layershell.InitForWindow(window)
-		gtk4layershell.SetLayer(window, gtk4layershell.LayerShellLayerOverlay)
-		gtk4layershell.SetKeyboardMode(window, gtk4layershell.LayerShellKeyboardModeNone)
-		gtk4layershell.SetAnchor(window, gtk4layershell.LayerShellEdgeTop, true)
-		gtk4layershell.SetAnchor(window, gtk4layershell.LayerShellEdgeLeft, true)
-		transparentWindow(window)
+}
 
-		var pic *gtk.Picture
-		fullImg, _, err := image.Decode(bytes.NewReader(imgBytes))
-		if err == nil {
-			spriteImg := cropImage(fullImg, 64, 0, 32, 32)
-			texture := imageToTexture(spriteImg)
-			if texture != nil {
-				pic = gtk.NewPictureForPaintable(texture)
-				pic.SetCanShrink(false)
-				pic.SetContentFit(gtk.ContentFitContain)
-				window.SetChild(pic)
-			}
-		} else {
-			log.Fatal(err)
+func run(window *winState) {
+	registry, err := window.display.GetRegistry()
+	if err != nil {
+		log.Fatalf("Unable to connect to Global registery! %v", err)
+	}
+	window.registry = registry
+	registry.AddGlobalHandler(window)
+	_ = wlclient.DisplayRoundtrip(window.display)
+}
+
+func (win *winState) HandleRegistryGlobal(r wl.RegistryGlobalEvent) {
+	switch r.Interface {
+	case "wl_shm": 
+		shm := wl.NewShm(win.display.Context())
+		err := win.registry.Bind(r.Name, r.Interface, r.Version, shm)
+		if err != nil {
+			log.Fatalf("Unable to bind wl_shm interface: %v", err)
 		}
-
-		var lastX int = 100
-		var lastY int = 100
-		var lastSpriteX int = 64
-		var lastSpriteY int = 0
-		glib.TimeoutAdd(125, func() bool {
-			cursorX, cursorY, err := hypr.GetCursorPos()
-			if err != nil {
-				log.Println(err)
-				return true
-			}
-
-			spriteX, spriteY, currX, currY := game.GetSpriteCoord(lastX, lastY, cursorX, cursorY)
-
-			if spriteX != lastSpriteX || spriteY != lastSpriteY {
-				spriteImg := cropImage(fullImg, spriteX, spriteY, 32, 32)
-				newTexture := imageToTexture(spriteImg)
-				pic.SetPaintable(newTexture)
-				lastSpriteX = spriteX
-				lastSpriteY = spriteY
-			}
-
-			if lastY != currY || lastX != currX {
-				gtk4layershell.SetMargin(window, gtk4layershell.LayerShellEdgeTop, min(currY, currY - 16))
-				gtk4layershell.SetMargin(window, gtk4layershell.LayerShellEdgeLeft, min(currX, currX - 16))
-				lastY = currY
-				lastX = currX
-			}
-
-			return true
-		})
-
-		window.Present()
-	})
-
-	if code := app.Run(os.Args); code > 0 {
-		os.Exit(code)
+		win.shm = shm
+	case "zwlr_layer_shell_v1":
+		zwlr := wlr.NewZwlrLayerShellV1(win.display.Context())
+		err := win.registry.Bind(r.Name, r.Interface, r.Version, zwlr)
+		if err != nil {
+			log.Fatalf("Unable to bind zwlr_layer_shell_v1: %v", err)
+		}
+		win.zwlr = zwlr
+	case "wl_compositor":
+		compositor := wl.NewCompositor(win.display.Context())
+		err := win.registry.Bind(r.Name, r.Interface, r.Version, compositor)
+		if err != nil {
+			log.Fatalf("Unable to bind wl_compositor: %v", err)
+		}
+		win.compositor = compositor
 	}
 }
