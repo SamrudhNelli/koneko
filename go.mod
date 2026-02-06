6d3bb7380c168694aeacef769cc86da351ee5942
Samrudh Nelli <samrudhnelli@gmail.com>
2026-02-06 20:29:46 +0530

    feat: render a transparent window in windows using lxn/win

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module koneko
 go 1.25.6
 
 require (
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e
 	github.com/neurlang/wayland v0.3.0
-	golang.org/x/sys v0.40.0
 )
 
-require github.com/yalue/native_endian v1.0.2 // indirect
+require (
+	github.com/yalue/native_endian v1.0.2 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+)
--- internal/render/windows/windows.go
+++ internal/render/windows/windows.go
@@ -1,68 +1,82 @@
 package windows
 
 import (
-	"log"
+	"runtime"
+	"syscall"
 	"unsafe"
 
-	"golang.org/x/sys/windows"
+	"github.com/lxn/win"
+)
+
+const (
+	AC_SRC_OVER = 0x00
+	ULW_ALPHA   = 0x00000002
 )
 
 var (
-	user32 = windows.NewLazySystemDLL("user32.dll")
-	user32GetSystemMetrics = user32.NewProc("GetSystemMetrics")
-	user32DefWindowProc = user32.NewProc("DefWindowProcW")
-	user32GetMessage = user32.NewProc("GetMessageW")
+	gdi32                   = syscall.NewLazyDLL("gdi32.dll")
+	procCreateDIBSection    = gdi32.NewProc("CreateDIBSection")
 )
 
-func runOnWindows() {
-	instance, err := windows.GetModuleHandle(nil)
-	if err != nil {
-		log.Fatalf("Unable to acces module handle: %v", err)
-	}
+type WindowsState struct {
+	window      win.HWND
+	MemDC     win.HDC
+	PixelData *[32 * 32 * 4]byte
+	Frames    [][]byte
+	Width     int32
+	Height    int32
+	ScreenDC  win.HDC
+}
 
-	className, _ := windows.UTF16PtrFromString("konekoClass")
-	wc := windows.WNDCLASSEX{
-		CbSize:        uint32(unsafe.Sizeof(windows.WNDCLASSEX{})),
-		LpfnWndProc:   windows.NewCallback(DefWindowProc),
+func init() {
+	runtime.LockOSThread()
+}
+
+func SetupWindows() *WindowsState {
+	state := &WindowsState{Width: 32, Height: 32}
+
+	instance := win.GetModuleHandle(nil)
+	className, _ := syscall.UTF16PtrFromString("konekoClass")
+	wc := win.WNDCLASSEX{
+		CbSize:        uint32(unsafe.Sizeof(win.WNDCLASSEX{})),
+		LpfnWndProc:   syscall.NewCallback(wndProc),
 		HInstance:     instance,
 		LpszClassName: className,
-		HbrBackground: windows.HBRUSH(windows.GetStockObject(windows.WHITE_BRUSH)),
+		HCursor:       win.LoadCursor(0, (*uint16)(unsafe.Pointer(uintptr(win.IDC_ARROW)))),
 	}
+	win.RegisterClassEx(&wc)
 
-	if errCode := windows.RegisterClassEx(&wc); errCode == 0 {
-		log.Fatal("Unable to register class!")
+	state.window = win.CreateWindowEx(
+		win.WS_EX_LAYERED|win.WS_EX_TOPMOST|win.WS_EX_TOOLWINDOW|win.WS_EX_TRANSPARENT,
+		className, nil,
+		win.WS_POPUP|win.WS_VISIBLE,
+		0, 0, 32, 32,
+		0, 0, instance, nil,
+	)
+
+	state.ScreenDC = win.GetDC(0)
+	state.MemDC = win.CreateCompatibleDC(state.ScreenDC)
+
+	bi := win.BITMAPINFOHEADER{
+		BiSize:        uint32(unsafe.Sizeof(win.BITMAPINFOHEADER{})),
+		BiWidth:       state.Width,
+		BiHeight:      -state.Height,
+		BiPlanes:      1,
+		BiBitCount:    32,
+		BiCompression: win.BI_RGB,
 	}
+	var bits unsafe.Pointer
+	hBitmap, _, _ := procCreateDIBSection.Call(uintptr(state.MemDC), uintptr(unsafe.Pointer(&win.BITMAPINFO{BmiHeader: bi})), 0, uintptr(unsafe.Pointer(&bits)), 0, 0,)
+	win.SelectObject(state.MemDC, win.HGDIOBJ(hBitmap))
+	state.PixelData = (*[32 * 32 * 4]byte)(bits)
 
-	winStyle := windows.WS_EX_LAYERED | windows.WS_EX_TRANSPARENT | windows.WS_EX_TOPMOST | windows.WS_EX_TOOLWINDOW
-	style := windows.WS_POPUP
-
-	screenWidth := getSystemMetrics(0)  // SM_CXSCREEN
-	screenHeight := getSystemMetrics(1) // SM_CYSCREEN
-
-	window, err := windows.CreateWindowEx(uint32(winStyle), className, nil, uint32(style), 0, 0, int32(screenWidth), int32(screenHeight), 0, 0, instance, nil)
-
-	if err != nil {
-		log.Fatalf("Unable to create a window: %v", err)
-	}
-
-	windows.ShowWindow(window, windows.SW_SHOW)
-
-	var msg windows.MSG
-	for {
-		if r, _, _ := user32GetMessage.Call(uintptr(unsafe.Pointer(&msg)), 0, 0, 0); r == 0 {
-			break
-		}
-		windows.TranslateMessage(&msg)
-		windows.DispatchMessage(&msg)
-	}
+	return state
 }
 
-func DefWindowProc(hwnd windows.HWND, msg uint32, wParam, lParam uintptr) uintptr {
-	r, _, _ := user32DefWindowProc.Call(uintptr(hwnd), uintptr(msg), wParam, lParam)
-	return r
-}
-
-func getSystemMetrics(index int) int {
-	ret, _, _ := user32GetSystemMetrics.Call(uintptr(index))
-	return int(ret)
+func wndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+	if msg == win.WM_DESTROY {
+		win.PostQuitMessage(0)
+		return 0
+	}
+	return win.DefWindowProc(hwnd, msg, wParam, lParam)
 }
