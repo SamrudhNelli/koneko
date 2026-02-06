0082d936b79e68115bf7032fb16ba0d941b700ab
Samrudh Nelli <samrudhnelli@gmail.com>
2026-02-06 20:43:48 +0530

    feat: allow rendering a sprite in the created window

--- internal/render/windows/windows.go
+++ internal/render/windows/windows.go
@@ -4,6 +4,11 @@ import (
 	"runtime"
 	"syscall"
 	"unsafe"
+	"log"
+	"image"
+	_ "image/png"
+	"image/draw"
+	"os"
 
 	"github.com/lxn/win"
 )
@@ -14,6 +19,8 @@ const (
 )
 
 var (
+	user32                   = syscall.NewLazyDLL("user32.dll")
+	procUpdateLayeredWindow = user32.NewProc("UpdateLayeredWindow")
 	gdi32                   = syscall.NewLazyDLL("gdi32.dll")
 	procCreateDIBSection    = gdi32.NewProc("CreateDIBSection")
 )
@@ -34,12 +41,13 @@ func init() {
 
 func SetupWindows() *WindowsState {
 	state := &WindowsState{Width: 32, Height: 32}
+	state.loadSprites("../assets/koneko.png")
 
 	instance := win.GetModuleHandle(nil)
 	className, _ := syscall.UTF16PtrFromString("konekoClass")
 	wc := win.WNDCLASSEX{
 		CbSize:        uint32(unsafe.Sizeof(win.WNDCLASSEX{})),
-		LpfnWndProc:   syscall.NewCallback(wndProc),
+		LpfnWndProc:   syscall.NewCallback(WndProc),
 		HInstance:     instance,
 		LpszClassName: className,
 		HCursor:       win.LoadCursor(0, (*uint16)(unsafe.Pointer(uintptr(win.IDC_ARROW)))),
@@ -73,7 +81,62 @@ func SetupWindows() *WindowsState {
 	return state
 }
 
-func wndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
+func (s *WindowsState) UpdatePosition(x, y, spriteX, spriteY int) {
+	spriteIndex := (spriteY - 1)*8 + (spriteX - 1)
+	copy(s.PixelData[:], s.Frames[spriteIndex])
+
+	ptDst := win.POINT{X: int32(x), Y: int32(y)}
+	ptSrc := win.POINT{X: 0, Y: 0}
+	size := win.SIZE{CX: s.Width, CY: s.Height}
+	blend := win.BLENDFUNCTION{BlendOp: AC_SRC_OVER, SourceConstantAlpha: 255, AlphaFormat: win.AC_SRC_ALPHA}
+
+	procUpdateLayeredWindow.Call(
+		uintptr(s.window), uintptr(s.ScreenDC), uintptr(unsafe.Pointer(&ptDst)), uintptr(unsafe.Pointer(&size)),
+		uintptr(s.MemDC), uintptr(unsafe.Pointer(&ptSrc)), 0, uintptr(unsafe.Pointer(&blend)), uintptr(ULW_ALPHA),
+	)
+}
+
+func (win *WindowsState) loadSprites(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Unable to open PNG: %v", err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		log.Fatalf("Unable to decode PNG: %v", err)
+	}
+
+	rgba := image.NewRGBA(img.Bounds())
+	draw.Draw(rgba, rgba.Bounds(), img, image.Point{}, draw.Src)
+
+	win.Frames = make([][]byte, 32)
+	for i := range win.Frames {
+		win.Frames[i] = make([]byte, 32*32*4)
+	}
+
+	for spriteY := 0; spriteY < 4; spriteY++ {
+		for spriteX := 0; spriteX < 8; spriteX++ {
+			for y := 0; y < 32; y++ {
+				for x := 0; x < 32; x++ {
+					spriteIndex := spriteY*8 + spriteX
+					pixOffset := rgba.PixOffset(spriteX*32+x, spriteY*32+y)
+					r, g, b, a := int(rgba.Pix[pixOffset]), int(rgba.Pix[pixOffset+1]), int(rgba.Pix[pixOffset+2]), int(rgba.Pix[pixOffset+3])
+					r, g, b = (r*a)/255, (g*a)/255, (b*a)/255
+
+					frameOffest := (y*32 + x) * 4
+					win.Frames[spriteIndex][frameOffest+0] = byte(b)
+					win.Frames[spriteIndex][frameOffest+1] = byte(g)
+					win.Frames[spriteIndex][frameOffest+2] = byte(r)
+					win.Frames[spriteIndex][frameOffest+3] = byte(a)
+				}
+			}
+		}
+	}
+}
+
+func WndProc(hwnd win.HWND, msg uint32, wParam, lParam uintptr) uintptr {
 	if msg == win.WM_DESTROY {
 		win.PostQuitMessage(0)
 		return 0
